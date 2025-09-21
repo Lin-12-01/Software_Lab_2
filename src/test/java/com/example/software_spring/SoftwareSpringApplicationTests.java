e768ebbc99e1625ed206f73dc82ec00b78acdbf0
Karim <karim.assimov@narxoz.kz>
2025-09-21 10:58:17 +0500

    Add main application class SoftwareSpringApplication

---
+++ src/test/java/com/example/software_spring/SoftwareSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.software_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
