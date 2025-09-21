fe32f144e431c281c1c4e5a51a4b184adca21c2c
Karim <karim.assimov@narxoz.kz>
2025-09-21 11:00:49 +0500

    Add main application class SoftwareSpringApplication №2

---
+++ src/main/java/com/example/software_spring/SoftwareSpringApplication.java
@@ -0,0 +1,12 @@
+package com.example.software_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareSpringApplication.class, args);
+    }
+
+}
